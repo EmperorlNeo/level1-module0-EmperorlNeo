c1d223362eb2a85956989e310a36720dfa73c5cf
EmperorlNeo <league@Mac-mini-4.local>
2018-06-02 10:40:17 -0700

    Fortune Cookie
    finished

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class FortuneCookie implements ActionListener {
 	public static void main(String[] args) {
@@ -15,6 +16,8 @@ public class FortuneCookie implements ActionListener {
 		JButton boton = new JButton();
 		frame.add(boton);
 		boton.addActionListener(fc);
+		boton.setText("Click For Fortune");
+		frame.setSize(300, 300);
 
 	}
 
@@ -27,10 +30,15 @@ public class FortuneCookie implements ActionListener {
 		// TODO Auto-generated method stub
 		Random cookie = new Random();
 		int c = cookie.nextInt(5);
-		JOptionPane.showMessageDialog(null, "Your fortune...");
+		JOptionPane.showMessageDialog(null, ".......");
+		JOptionPane.showMessageDialog(null, "...I'm thinking...");
+		JOptionPane.showMessageDialog(null, "...uuhh...");
+		JOptionPane.showMessageDialog(null, ".......");
+		JOptionPane.showMessageDialog(null, ".......");
+		JOptionPane.showMessageDialog(null, "OH, I got it!➟");
 		if (c == 0) {
 			JOptionPane.showMessageDialog(null,
-					"Come back later...I am sleeping. (yes, cookies need their sleep too).  Lucky Numbers 28, 33, 46, 5, 15, 2");
+					"Come back later...I am sleeping. (Yes, cookies need their sleep too).  Lucky Numbers 28, 33, 46, 5, 15, 2");
 		}
 		if (c == 1) {
 			JOptionPane.showMessageDialog(null,
@@ -40,7 +48,10 @@ public class FortuneCookie implements ActionListener {
 			JOptionPane.showMessageDialog(null, "You are not illiterate.  Lucky Numbers 49, 35, 6, 11, 17, 42");
 		}
 		if (c == 3) {
-			JOptionPane.showMessageDialog(null,"");
+			JOptionPane.showMessageDialog(null,"Some fortune cookie contain no fortune...  Lucky Numbers 34, 7, 8, 23, 10");
+		}
+		if (c == 4) {
+			JOptionPane.showMessageDialog(null, "That wasn't chicken...  Lucky Numbers 25, 5, 9, 17, 37, 44");
 		}
 	}
 }
