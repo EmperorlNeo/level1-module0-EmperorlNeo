0dc7186e431173e1c3e158e1b7bed875f6f42d75
EmperorlNeo <neomartindelcampo2001@gmail.com>
2018-06-23 11:27:51 -0700

    Cuteness TV and Sound Effects Machine
    Finished both

---
+++ src/CutenessTV.java
@@ -0,0 +1,72 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CutenessTV implements ActionListener {
+	public static void main(String[] args) {
+		new CutenessTV().createUI();
+	}
+
+	private void createUI() {
+		frame.setVisible(true);
+		frame.setSize(200, 200);
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		button1.setText("Ducks");
+		button2.setText("Frog");
+		button3.setText("Fluffy Unicorns");
+	}
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+
+	void showDucks() {
+		playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+		playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+		try {
+			URI uri = new URI(videoID);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed == button1) {
+			showDucks();
+		}
+		if (buttonPressed == button2) {
+			showFrog();
+		}
+		if (buttonPressed == button3) {
+			showFluffyUnicorns();
+		}
+	}
+
+}
--- src/SoundEffectsMachine.java
+++ src/SoundEffectsMachine.java
@@ -11,43 +11,58 @@ public class SoundEffectsMachine implements ActionListener {
 	JButton button = new JButton();
 
 	public static void main(String[] args) {
-		SoundEffectsMachine sem = new SoundEffectsMachine();
-//Input the UI				
-		JFrame frame = new JFrame();
+		new SoundEffectsMachine().createUI();
+
+	}
+
+	private void createUI() {
+
 		frame.setVisible(true);
-		JPanel panel = new JPanel();
+
 		frame.add(panel);
 		frame.setSize(70, 270);
 		frame.setLocation(700, 500);
-		
-		panel.add(button);
-		button.setText("Ballon Pop");
 
-		JButton button2 = new JButton();
 		panel.add(button2);
+		button2.addActionListener(this);
 		button2.setText("Glass Break");
 
-		JButton button3 = new JButton();
 		panel.add(button3);
+		button3.addActionListener(this);
 		button3.setText("Car Screech");
 
-		JButton button4 = new JButton();
 		panel.add(button4);
-		button4.setText(" Screams ");
+		button4.addActionListener(this);
+		button4.setText(" Scream ");
 
-		JButton button5 = new JButton();
 		panel.add(button5);
+		button5.addActionListener(this);
 		button5.setText("Applause");
 
-		JButton button6 = new JButton();
 		panel.add(button6);
+		button6.addActionListener(this);
 		button6.setText("Baby Laugh");
 
-		JButton button7 = new JButton();
 		panel.add(button7);
+		button7.addActionListener(this);
 		button7.setText("Fireworks");
 
+		panel.add(button8);
+		button8.addActionListener(this);
+		button8.setText("Ballon Pop");
 	}
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JButton button4 = new JButton();
+	JButton button5 = new JButton();
+	JButton button6 = new JButton();
+	JButton button7 = new JButton();
+	JButton button8 = new JButton();
+
 	private void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
@@ -57,6 +72,28 @@ public class SoundEffectsMachine implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
-
+		// System.out.println("press");
+		if (buttonPressed == button2) {
+			// System.out.println("Glass");
+			playSound("GlassBreak.wav");
+		}
+		if (buttonPressed == button3) {
+			playSound("CarScreech.wav");
+		}
+		if (buttonPressed == button4) {
+			playSound("Screams.wav");
+		}
+		if (buttonPressed == button5) {
+			playSound("Applause.wav");
+		}
+		if (buttonPressed == button6) {
+			playSound("BabyLaugh.wav");
+		}
+		if (buttonPressed == button7) {
+			playSound("Fireworks.wav");
+		}
+		if (buttonPressed == button8) {
+			playSound("BallonPop.wav");
+		}
 	}
 }
